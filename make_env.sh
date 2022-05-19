4bc9e9c9d4fe9c470a579c9510044c403c0361a2
OksanichenkoFedor <okssolotheodor@gmail.com>
2022-05-19 16:18:42 +0300

    Add bash script for making data environment

---
+++ make_env.sh
@@ -0,0 +1,30 @@
+
+mkdir Data
+cd Data
+mkdir BadPure
+mkdir BadZipped
+mkdir Converted
+mkdir Downloads
+mkdir Generated Converted
+mkdir Generated Pure
+mkdir Generated Zipped
+mkdir Zipped
+mkdir Tokenisers
+mkdir InfoFiles
+cd InfoFiles
+touch NumOpen.txt
+touch unzipped.txt
+touch gzipped.txt
+touch gpure.txt
+touch downloaded.txt
+touch converted.txt
+touch badzipped.txt
+touch bad_dl.txt
+cd ../
+cd ../
+mkdir logs
+cd logs
+touch downloading.txt
+touch converting.txt
+touch full_log.txt
+touch info.pickle
